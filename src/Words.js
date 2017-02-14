555459a189f7994544bcdc13964ad0b50a65ff3e
idem <victoridongesit@gmail.com>
2017-02-14 11:24:26 +0100

    words, 10 tests passed successfully

--- src/Words.js
+++ src/Words.js
@@ -2,34 +2,33 @@ function Words(){
 	this.words = function(inputWord){
 		// Split the words into an array, using a regular expression
 		var wordArray = inputWord.match(/\S+/g);
-		// Create and empty JSON object
-		var result = [];
-		var count = [];
+		// Create an array for holding the JSON object keys and values
+		var key = [];
+		var value = [];
 		var k = wordArray.length;
+		// Count word occurence
 		for(var i = 0; i < k; i++){
 			var word = wordArray[i];
 			var counter = 1;
 			if(word !== null){
-			  result.push(word);
+			  key.push(word);
 			}
 			for(var j = i+1; j <= k; j++){
 				if(wordArray[i] === wordArray[j] && wordArray[j] !== null){
 					counter +=1;
+					// Remove found word
 					wordArray[j] = null;
 				}
 			}
 			if(word !== null){
-			  count.push(counter);
+			  value.push(counter);
 			}
 			k = wordArray.length;
 		}
 		var output = {};
-		for(var m = 0; m < result.length; m++){
-			var st = result[m];
-			output.st = count[m];
+		for(var m = 0; m < key.length; m++){
+			output[key[m]] = value[m];
 		}
 		return output;
-		//console.log(result);
-		//console.log(count);
 	};
 }
