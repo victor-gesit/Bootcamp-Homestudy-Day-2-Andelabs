556dc390975c00bf3145e791169bc38a5339af1b
idem <victoridongesit@gmail.com>
2017-02-14 05:52:07 +0100

    Word count algorithm development

--- src/Words.js
+++ src/Words.js
@@ -1,5 +1,17 @@
 function Words(){
-	this.words = function(){
-
+	this.words = function(words){
+		// Split the words into an array, using regular expressions
+		wordArray = words.match(/\S+/g);
+		// Create and empty JSON object
+		var result = [];
+		var k = wordArray.length;
+		for(var i = 0; i < k; i++){
+			var word = wordArray[i];
+			var count = 0;
+			for(var j = i; j < k; j++){
+				if(wordArray[i])
+			}
+			k = wordArray.length;
+		}
 	};
 }
