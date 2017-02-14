9c12118533b498f63ea52de7d57a1bd770e82f33
idem <victoridongesit@gmail.com>
2017-02-14 06:50:51 +0100

    Word count algorithm, more progress

--- src/Words.js
+++ src/Words.js
@@ -1,17 +1,35 @@
 function Words(){
-	this.words = function(words){
-		// Split the words into an array, using regular expressions
-		wordArray = words.match(/\S+/g);
+	this.words = function(inputWord){
+		// Split the words into an array, using a regular expression
+		var wordArray = inputWord.match(/\S+/g);
 		// Create and empty JSON object
 		var result = [];
+		var count = [];
 		var k = wordArray.length;
 		for(var i = 0; i < k; i++){
 			var word = wordArray[i];
-			var count = 0;
-			for(var j = i; j < k; j++){
-				if(wordArray[i])
+			var counter = 1;
+			if(word !== null){
+			  result.push(word);
+			}
+			for(var j = i+1; j <= k; j++){
+				if(wordArray[i] === wordArray[j] && wordArray[j] !== null){
+					counter +=1;
+					wordArray[j] = null;
+				}
+			}
+			if(word !== null){
+			  count.push(counter);
 			}
 			k = wordArray.length;
 		}
+		var output = {};
+		for(var m = 0; m < result.length; m++){
+			var st = result[m];
+			output.st = count[m];
+		}
+		return output;
+		//console.log(result);
+		//console.log(count);
 	};
 }
